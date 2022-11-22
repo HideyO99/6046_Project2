3876091718271cbd4afc6bd03c1b7f44aeedea36
Siraphong <hideyo99@hotmail.com>
2022-11-22 14:31:52 -0500

    merge fmod in main

--- 6046_Project2/src/main.cpp
+++ 6046_Project2/src/main.cpp
@@ -23,9 +23,27 @@
 #include "Light/cLightManager.h"
 #include "GUI/cGUI.h"
 
+//sound
+#include "FMOD/FmodManager.h"
+#include "XML/cXML.h"
+#include "FMOD/Sound_UI.h"
+
+#define MAX_CHANNEL 255
+#define MASTER_CH	"Master"
+#define BGM_CH1		"BGM1"
+#define BGM_CH2		"BGM2"
+#define BGM_CH3		"BGM3"
+#define	FX1_CH		"fx1"
+#define FX2_CH		"fx2"
+
+//graphic
 #define MODEL_LIST_XML          "asset/model.xml"
 #define VERTEX_SHADER_FILE      "src/shader/vertexShader.glsl"
 #define FRAGMENT_SHADER_FILE    "src/shader/fragmentShader.glsl"
+
+cXML xml;
+FModManager fmodmanager;
+Sound_UI sound_UI(&fmodmanager, &xml);
 
 glm::vec3 g_cameraEye = glm::vec3(0.0, 5.0, 0.0f);
 glm::vec3 g_cameraTarget = glm::vec3(6.0f, 0.0f, 0.0f);
@@ -58,6 +76,7 @@ void light1Setup();
 void light2Setup();
 void light3Setup();
 void light4Setup();
+bool FMOD_setup();
 
 int main(void)
 {
@@ -228,26 +247,7 @@ int main(void)
     light3Setup();
     light4Setup();
 
-    //result = pVAOManager->setInstanceObjWireframe("sphere01", true);
-    //result = pVAOManager->setInstanceObjWireframe("sphere01", true);
-
-    ////result = pVAOManager->setInstanceObjVisible("sphere02", true);
-    //result = pVAOManager->setInstanceObjRGB("sphere02", glm::vec4(1.f, 0.f, 0.f, 1.f));
-    //result = pVAOManager->setInstanceObjWireframe("sphere02", true);
-
-    ////result = pVAOManager->setInstanceObjVisible("sphere03", true);
-    //result = pVAOManager->setInstanceObjRGB("sphere03", glm::vec4(0.f, 1.f, 0.f, 1.f));
-    //result = pVAOManager->setInstanceObjWireframe("sphere03", true);
-
-    ////result = pVAOManager->setInstanceObjVisible("sphere04", true);
-    //result = pVAOManager->setInstanceObjRGB("sphere04", glm::vec4(0.f, 0.f, 1.f, 1.f));
-    //result = pVAOManager->setInstanceObjWireframe("sphere04", true);
-
-    ////result = pVAOManager->setInstanceObjVisible("sphere05", true);
-    //result = pVAOManager->setInstanceObjRGB("sphere05", glm::vec4(0.f, 1.f, 1.f, 1.f));
-    //result = pVAOManager->setInstanceObjWireframe("sphere05", true);
-    ////result = pVAOManager->setInstanceObjVisible("terrain02", true);
-    ////result = pVAOManager->setInstanceObjScale("terrain02", 0.5);
+    result = FMOD_setup();
 
     while (!glfwWindowShouldClose(window))
     {
@@ -297,8 +297,11 @@ int main(void)
 
     }
 
+    //release FMOD allocation
+    fmodmanager.shutdown();
+    //release IMGUI allocation
     gui_->ImGUI_shutdown();
-
+    //release window allocation
     glfwDestroyWindow(window);
 
     delete pVAOManager;
@@ -532,6 +535,65 @@ void light4Setup()
     ::g_pTheLightManager->plight[9]->angle.x = 10.0f;     // Degrees
     ::g_pTheLightManager->plight[9]->angle.y = 20.0f;     // Degrees
     ::g_pTheLightManager->plight[9]->turnON = 1;
+}
+
+bool FMOD_setup()
+{
+    bool result;
+    //init fmod
+    result = fmodmanager.Fmod_init(MAX_CHANNEL, FMOD_INIT_NORMAL);
+    if (!result)
+    {
+        return -2;
+    }
+
+    result = fmodmanager.create_channel(MASTER_CH);
+    result = fmodmanager.create_channel(BGM_CH1);
+    result = fmodmanager.create_channel(BGM_CH2);
+    result = fmodmanager.create_channel(BGM_CH3);
+    result = fmodmanager.create_channel(FX1_CH);
+    result = fmodmanager.create_channel(FX2_CH);
+    if (!result)
+    {
+        return -3;
+    }
+
+    result = fmodmanager.set_channel_parent(BGM_CH1, MASTER_CH);
+    result = fmodmanager.set_channel_parent(BGM_CH2, MASTER_CH);
+    result = fmodmanager.set_channel_parent(BGM_CH3, MASTER_CH);
+    result = fmodmanager.set_channel_parent(FX1_CH, MASTER_CH);
+    result = fmodmanager.set_channel_parent(FX2_CH, MASTER_CH);
+    if (!result)
+    {
+        return -4;
+    }
+
+    //read XML file
+    xml.readXML();
+
+    result = fmodmanager.set_channel_vol(MASTER_CH, 0.5f);
+    //result = fmodmanager.create_stream("bgm1", xml.my_music[0], FMOD_LOOP_NORMAL, false);
+    //result = fmodmanager.create_stream("bgm2", xml.my_music[1], FMOD_LOOP_NORMAL, false);
+    //result = fmodmanager.create_stream("bgm3", xml.my_music[2], FMOD_LOOP_NORMAL, false);
+    result = fmodmanager.create_sound("fx1", xml.my_fx_path[0], FMOD_DEFAULT, false);
+    result = fmodmanager.create_sound("fx2", xml.my_fx_path[1], FMOD_DEFAULT, false);
+    result = fmodmanager.create_sound("fx3", xml.my_fx_path[2], FMOD_DEFAULT, false);
+    result = fmodmanager.create_sound("fx4", xml.my_fx_path[3], FMOD_DEFAULT, false);
+    result = fmodmanager.create_sound("fx5", xml.my_fx_path[4], FMOD_DEFAULT, false);
+    result = fmodmanager.create_sound("fx6", xml.my_fx_path[5], FMOD_DEFAULT, false);
+    result = fmodmanager.create_sound("fx7", xml.my_fx_path[6], FMOD_DEFAULT, false);
+    //result = fmodmanager.play_sound("bgm1", BGM_CH1);
+
+    result = fmodmanager.create_dsp(DSP_ECHO, FMOD_DSP_TYPE_ECHO);
+    result = fmodmanager.create_dsp(DSP_DISTORTION, FMOD_DSP_TYPE_DISTORTION);
+    result = fmodmanager.create_dsp(DSP_CHORUS, FMOD_DSP_TYPE_CHORUS);
+    result = fmodmanager.create_dsp(DSP_LOWPASSFILTER, FMOD_DSP_TYPE_LOWPASS);
+    result = fmodmanager.create_dsp(DSP_HIGHPASSFILTER, FMOD_DSP_TYPE_HIGHPASS);
+    result = fmodmanager.create_dsp(DSP_FADER, FMOD_DSP_TYPE_FADER);
+    result = fmodmanager.create_dsp(DSP_PITCHSHIFT, FMOD_DSP_TYPE_PITCHSHIFT);
+    result = fmodmanager.create_dsp(DSP_FLANGE, FMOD_DSP_TYPE_FLANGE);
+    result = fmodmanager.create_dsp(DSP_TREMELO, FMOD_DSP_TYPE_TREMOLO);
+    return result;
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
