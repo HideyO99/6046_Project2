d7ccdb2ce8d894de01e33b0455dda01a38b49eef
Siraphong <hideyo99@hotmail.com>
2022-11-22 14:14:31 -0500

    add fmod

--- 6046_Project2/src/XML/cXML.cpp
+++ 6046_Project2/src/XML/cXML.cpp
@@ -10,6 +10,75 @@ cXML::~cXML()
 {
 }
 
+void cXML::readXML()
+{
+	pugi::xml_document music_xml;
+	pugi::xml_parse_result result = music_xml.load_file("sound_list.xml");
+	if (!result)
+	{
+		std::cout << "Unable to load XML file" << std::endl;
+		return;
+	}
+	std::cout << "Loaded XML" << std::endl;
+	pugi::xml_object_range<pugi::xml_node_iterator> music = music_xml.child("music").children();
+	if (music.empty())
+	{
+		std::cout << "There are no entries" << std::endl;
+		return;
+	}
+
+	pugi::xml_node_iterator music_i;
+	int i = 0;
+	int j = 0;
+	for (music_i = music.begin(); music_i != music.end(); ++music_i)
+	{
+		pugi::xml_node fname = *music_i;
+		pugi::xml_node_iterator musicit;
+		//std::string s = fname.child_value();
+		my_music[i].fname = fname.child_value();
+		my_music[i].fname.erase(std::remove(my_music[i].fname.begin(), my_music[i].fname.end(), '\n'), my_music[i].fname.cend());	//remove \n
+		my_music[i].fname.erase(std::remove(my_music[i].fname.begin(), my_music[i].fname.end(), '\t'), my_music[i].fname.cend());	//remove \t
+		for (musicit = fname.children().begin(); musicit != fname.children().end(); ++musicit)
+		{
+			pugi::xml_node pathNode = *musicit;
+
+			if (!pathNode.children().empty())
+			{
+				my_music[i].path[j] = pathNode.child_value();
+				j++;
+			}
+		}
+		i++;
+		j = 0;
+	}
+
+	pugi::xml_object_range<pugi::xml_node_iterator> fx = music_xml.child("fx").children();
+	if (fx.empty())
+	{
+		std::cout << "There are no entries" << std::endl;
+		return;
+	}
+
+	pugi::xml_node_iterator fx_i;
+	i = j = 0;
+
+	for (fx_i = fx.begin(); fx_i != fx.end(); fx_i++)
+	{
+		pugi::xml_node fx_path = *fx_i;
+		pugi::xml_node_iterator fx_it;
+		for (fx_it = fx_path.children().begin(); fx_it != fx_path.children().end(); fx_it++)
+		{
+			pugi::xml_node pathNode = *fx_it;
+			if (!pathNode.children().empty())
+			{
+				my_fx_path[i] = pathNode.child_value();
+				i++;
+			}
+		}
+	}
+	//music_xml.~xml_document();
+}
+
 bool cXML::loadModelFromXML(std::string filename, std::map<std::string, cMeshObj*>& mapInstancetoMeshOBJ, std::vector<cMeshObj*>& pVecInstanceMeshObj)
 {
 	pugi::xml_document modelList_xml;
@@ -101,4 +170,10 @@ bool cXML::findPathFromModelName(std::string modelName, std::string& modelPath)
 	modelPath = i_model->second;
 
 	return true;
+}
+
+cXML::MyMusic::MyMusic()
+{
+	path[0] = "";
+	path[1] = "";
 }
--- 6046_Project2/src/XML/cXML.h
+++ 6046_Project2/src/XML/cXML.h
@@ -13,11 +13,26 @@ public:
 	cXML();
 	~cXML();
 
+	//media section
+	struct MyMusic
+	{
+		std::string fname;
+		std::string path[2];
+
+		MyMusic();
+	};
+
+	void readXML();
+
+	//graphic section
 	bool loadModelFromXML(std::string filename, std::map<std::string, cMeshObj*>& mapInstancetoMeshOBJ, std::vector<cMeshObj*>& pVecInstanceMeshObj);
 	bool findPathFromModelName(std::string modelName, std::string& modelPath);
-
+	//graphic section
 	std::map<std::string, std::string> mapModelNameAndPath;
 	std::map<std::string, std::string> mapInstanceAndModelName;
 	glm::vec3 cameraEyeFromXML;
+	//media section
+	MyMusic my_music[3];
+	std::string my_fx_path[7];
 };
 
